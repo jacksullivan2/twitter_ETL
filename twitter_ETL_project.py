16c5f143786a4c43a0730cb77a9bb03fefd06f16
jacksullivan2 <44735471+jacksullivan2@users.noreply.github.com>
2021-06-10 14:51:51 +0100

    Add files via upload

--- twitter_ETL_project.py
+++ twitter_ETL_project.py
@@ -2,6 +2,8 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler 
 from tweepy import Stream 
 from twitter_credentials import *
+import json 
+from datetime import datetime
 
 ## Twitter Authenticator class ##
 class Authenticator():
@@ -20,14 +22,14 @@ class TwitterStreamer():
 
 	def stream_tweets(self, filename, key_words):
 		"""Method that handles twitter authentication and connection to the Twitter Streaming API"""
-		listener = TwitterListener(filename)
+		listener = MyListener(filename)
 		twitter_authenticator = Authenticator()
 		auth = twitter_authenticator.authenticate_twitter_app()
 		stream = Stream(auth, listener)
 		stream.filter(track=key_words)
 
 
-class TwitterListener(StreamListener):
+class MyListener(StreamListener):
 	"""Class that prints out tweets or error assoociated with a tweet"""
 
 	def __init__(self, filename):
@@ -35,28 +37,37 @@ class TwitterListener(StreamListener):
 		self.filename = filename
 
 	def on_data(self, data):
-		"""Override this method from the parent class to print out the data
-		that we get from the StreamListener class and write the printed tweets to self.filename file. 
+		"""Override this method from the parent class. This method is called automatically whenever data arrives to the listener.
+		You need to return True to keep the stream going. If you return False, it stops the stream.
+		Before returning True or False, you can do whatever you want with the data.   
 		"""
-		try:
-			with open(self.filename, 'a') as file_object:
+		with open(self.filename, 'a') as file_object:
 				file_object.write(data)
-			return True
-		except BaseException as e:
-			pass
+		
+		tweet_dict = json.loads(data)
+		time = tweet_dict['created_at']
+		tweet = tweet_dict['text']
+		print(time, tweet)
+		print()
+
 		return True
 
 
 	def on_error(self, status):
-		"""Override this method from the parent class to print out the error associated with 
-		data/tweet receieved from  StreamListener with an error"""
-		print(status) 
+		"""Override this method from the parent class. Here we listen for status code 420, if you get this status code then you have to close the
+		stream because if something is going wrong, the stream will continue to connecting to the Twitter API. If you don't close the stream 
+		after an error has occured you could get banned from accessing the API. 
+		"""
+		if status == 420:
+			return False
 
 
 if __name__ == '__main__':
 
 	key_words = ['#bitcoin']
-	filename = 'cryptocurrency_tweets.json'
+	filename = 'bitcoin.json'
 
 	twitter_stream = TwitterStreamer()
 	twitter_stream.stream_tweets(filename, key_words)
+
+
